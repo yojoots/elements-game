d0d5286f385a40a9fad527228e06e0503318cb75
Justin Hanneman <yojoots@gmail.com>
2024-03-18 21:54:05 -0500

    verbose, battles working

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const initPlayer = (userId) => {
         color: stringToColour(userId),
         lastSpellCastInRound: -1,
         isScrying: false,
+        attacking: "",
+        order: "aefw",
         life: 2000,
         air: 0,
         earth: 0,
@@ -37,6 +39,7 @@ const initGame = (roomId) => {
         players: [],
         maxPlayers: 10,
         potSize: 80.00,
+        weather: "clear",
         roundDuration: 30
     }
 }
@@ -48,6 +51,7 @@ const defaultGameState = {
     players: [],
     maxPlayers: 10,
     potSize: 80.00,
+    roundCount: 20,
     roundDuration: 30
 };
 
@@ -78,6 +82,20 @@ function saveGameState(gameState) {
           console.log("WROTE GAMESTATE:", gameState.roomId)
         }
     });
+}
+
+function letterToElement(letter) {
+    if (letter === "a") {
+        return "air"
+    } else if (letter === "e") {
+        return "earth"
+    } else if (letter === "f") {
+        return "fire"
+    } else if (letter === "w") {
+        return "water"
+    } else {
+        return "neutral"
+    }
 }
 
 function hashCode(string){
@@ -148,6 +166,16 @@ io.on('connection', (socket) => {
             return;
         }
 
+        if (args.text.startsWith("/attack")) {
+            let playerToAttack = args.text.split(" ")[1];
+            player.attacking = playerToAttack;
+        }
+
+        if (args.text.startsWith("/order")) {
+            let newOrder = args.text.split(" ")[1];
+            player.order = newOrder;
+        }
+
         //io.to(roomId).emit('newRound', {round: gameState.round})
         //io.to(userId).emit('foo', {room: roomId, text: args.text, user: userId, id: args.userName + args.text, key: args.userName + args.text})
         let timeString = new Date().getTime().toString();
@@ -181,6 +209,50 @@ io.on('connection', (socket) => {
         }
 
         player.nickname = args.nickname;
+
+        //io.to(roomId).emit('setNickname', {room: roomId, nickname: player.nickname, user: userId})
+        saveGameState(gameState);
+        io.to(userId).emit('playerState', {room: roomId, user: userId, playerState: player});
+    });
+
+    socket.on("updateOrder", (args) => {
+        console.log("GOT REORDER WITH ARGUS:", args);
+
+        if (!args.order) {
+            return
+        }
+
+        if (args.order.length != 4) {
+            return
+        }
+
+        if (!(args.order.indexOf("a") > -1 && args.order.indexOf("e") > -1 && args.order.indexOf("f") > -1 && args.order.indexOf("w") > -1)) {
+            return
+        }
+
+        if (!(args.roomId && args.userUid)) {
+            return
+        }
+        let roomId = args.roomId;
+        let userId = args.userUid;
+        let gameState = knownGameStates[roomId];
+        console.log("GAME STATE:", gameState);
+        if (gameState === undefined) {
+            return
+        }
+        let userExists = arrContains(gameState.players, userId);
+
+        if (!userExists && gameState.round === 0) {
+            // Auto-join for now
+            gameState.players.push(initPlayer(userId));
+        }
+        player = gameState.players.find((p) => {return p.id == userId});
+
+        if (player == undefined) {
+            return;
+        }
+
+        player.order = args.order;
 
         //io.to(roomId).emit('setNickname', {room: roomId, nickname: player.nickname, user: userId})
         saveGameState(gameState);
@@ -339,9 +411,8 @@ io.on('connection', (socket) => {
             knownGameStates[roomId] = initGame(roomId);
             gameState = knownGameStates[roomId]
         }
-        gameState.round = gameState.round + 1;
+        processRoundAndProceed(roomId);
         console.log(`Manually proceeding to round ${gameState.round} in game ID: ${roomId}`);
-        io.to(roomId).emit('newRound', {round: gameState.round})
     });
 
     socket.on('disconnect', () => {
@@ -349,13 +420,220 @@ io.on('connection', (socket) => {
     });
 });
 
+function combat(attackingArmy, defendingArmy, weather) {
+    let attackMultiplier = 1
+    let defendMultiplier = 1
+
+    if (attackingArmy.element == "air" && defendingArmy.element == "earth") {
+        defendMultiplier /= 2
+    } else if (attackingArmy.element == "air" && defendingArmy.element == "fire") {
+        attackMultiplier /= 2
+    } else if (attackingArmy.element == "earth" && defendingArmy.element == "air") {
+        attackMultiplier /= 2
+    } else if (attackingArmy.element == "earth" && defendingArmy.element == "water") {
+        defendMultiplier /= 2
+    } else if (attackingArmy.element == "fire" && defendingArmy.element == "air") {
+        defendMultiplier /= 2
+    } else if (attackingArmy.element == "fire" && defendingArmy.element == "water") {
+        attackMultiplier /= 2
+    } else if (attackingArmy.element == "water" && defendingArmy.element == "earth") {
+        attackMultiplier /= 2
+    } else if (attackingArmy.element == "water" && defendingArmy.element == "fire") {
+        defendMultiplier /= 2
+    }
+
+    if (weather == "tornado") {
+        if (attackingArmy.element == "air") {
+            defendMultiplier /= 2
+        }
+        if (defendingArmy.element == "air") {
+            attackMultiplier /= 2
+        }
+    } else if (weather == "flood") {
+        if (attackingArmy.element == "water") {
+            defendMultiplier /= 2
+        }
+        if (defendingArmy.element == "water") {
+            attackMultiplier /= 2
+        }
+    } else if ( weather == "comet") {
+        if (attackingArmy.element == "fire") {
+            defendMultiplier /= 2
+        }
+        if (defendingArmy.element == "fire") {
+            attackMultiplier /= 2
+        }
+    } else if ( weather == "earthquake") {
+        if (attackingArmy.element == "earth") {
+            defendMultiplier /= 2
+        }
+        if (defendingArmy.element == "earth") {
+            attackMultiplier /= 2
+        }
+    }
+
+    totalAttackScore = attackingArmy.size * attackMultiplier
+    totalDefendScore = defendingArmy.size * defendMultiplier
+
+    console.log(`Total (${attackingArmy.element}) Attack Score: ${totalAttackScore}`)
+    console.log(`Total (${defendingArmy.element}) Defend Score: ${totalDefendScore}`)
+
+    if (totalAttackScore > totalDefendScore) {
+        // Attacker wins
+        return {
+            "attackRemaining": totalAttackScore - totalDefendScore,
+            "defendRemaining": 0
+        }
+    } else if (totalDefendScore > totalAttackScore) {
+        // Defender wins
+        return {
+            "attackRemaining": 0,
+            "defendRemaining": totalDefendScore - totalAttackScore
+        }
+    } else {
+        return {
+            "attackRemaining": 0,
+            "defendRemaining": 0
+        }
+    }
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
+
+function generateWeather() {
+    let weatherSeed = getRandomInt(100);
+    weather = ""
+    if (weatherSeed < 60) {
+        weather = "Clear"
+    } else if(weatherSeed < 70) {
+        weather = "Tornado"
+    } else if (weatherSeed < 80) {
+        weather = "Comet"
+    } else if (weatherSeed < 90) {
+        weather = "Flood"
+    } else {
+        weather = "Earthquake"
+    }
+
+    return weather
+}
+
+function topTroops(player, attackingOrDefending) {
+    // Prioritize empower and fortify buffs first
+    if (attackingOrDefending == "attack" && player.empower > 0) {
+        return {troopType: "empower", amount: player.empower}
+    }
+
+    // Prioritize empower and fortify buffs first
+    if (attackingOrDefending == "defend" && player.fortify > 0) {
+        return {troopType: "fortify", amount: player.fortify}
+    }
+
+    // Return first nonzero element from player's troop ordering
+    for (let c = 0; c < player.order.length; c++) {
+        let troopType = letterToElement(player.order.charAt(c));
+        if (player[troopType] > 0) {
+            return {troopType: troopType, amount: player[troopType]}
+        }
+    }
+            
+    return {troopType: "none", amount: 0}
+}
+
+function processRoundAndProceed(roomId) {
+    console.log("PROCESSING ROUND AND PROCEEDING FOR:", roomId);
+    gameState = knownGameStates[roomId];
+
+    let playerTurns = [...Array(gameState.players.length).keys()];
+    shuffle(playerTurns);
+    console.log("PLAYER TURNS:", playerTurns);
+
+    for (turn of playerTurns) {
+        playingPlayer = gameState.players[turn];
+        console.log("TURN", turn);
+        console.log("PLAYING PLAYER:", playingPlayer);
+        if (playingPlayer.attacking.length > 0) {
+            let defendingPlayer = gameState.players[parseInt(playingPlayer.attacking)];
+            if (defendingPlayer.id === playingPlayer.id) {
+                // Can't attack yourself, silly
+                return;
+            }
+            // Execute attack
+            console.log("ATTACKING THE DEFENDER:", defendingPlayer);
+            let attackingStrengthAndType = topTroops(playingPlayer, "attack");
+            let defendingStrengthAndType = topTroops(defendingPlayer, "defend");
+            while (attackingStrengthAndType.amount > 0 && defendingStrengthAndType.amount > 0) {
+                battleResults = combat({size: attackingStrengthAndType.amount, element: attackingStrengthAndType.troopType}, {size: defendingStrengthAndType.amount, element: defendingStrengthAndType.troopType}, gameState.weather);
+                
+                playingPlayer[attackingStrengthAndType.troopType] = battleResults.attackRemaining;
+                defendingPlayer[defendingStrengthAndType.troopType] = battleResults.defendRemaining;
+
+                let timeString = new Date().getTime().toString();
+                let battleResultsHash = hashCode(JSON.stringify(battleResults) + timeString);
+                io.to(roomId).emit('newMessage', {room: roomId, text: JSON.stringify(battleResults), user: "SYSTEM", color: "BLACK", id: battleResultsHash, key: battleResultsHash});
+                attackingStrengthAndType = topTroops(playingPlayer, "attack");
+                defendingStrengthAndType = topTroops(defendingPlayer, "defend");
+            }
+
+            if (attackingStrengthAndType.amount > 0) {
+                console.log("ATTACKER WINS!");
+                let totalLooters = playingPlayer.empower + playingPlayer.air + playingPlayer.earth + playingPlayer.fire + playingPlayer.water;
+                let lootableValue = Math.min(totalLooters, defendingPlayer.life);
+                defendingPlayer.life -= lootableValue;
+                playingPlayer.life += lootableValue;
+                console.log("LOOTED LIFE:", lootableValue);
+            } else if (defendingStrengthAndType.amount > 0) {
+                console.log("DEFENDER WINS!");
+            } else {
+                console.log("MUTUAL DESTRUCTION!");
+            }
+        }
+    }
+
+    // Now double everyone's life and alert them of playerState changes
+    for (const player of gameState.players) {
+        player.life = player.life * 2;
+        player.attacking = "";
+        player.isScrying = false;
+        io.to(player.id).emit('playerState', {room: roomId, user: player.id, playerState: player});
+    }
+
+    gameState.weather = generateWeather();
+
+    if (gameState.round >= gameState.roundCount) {
+        io.to(roomId).emit('gameResults', {game: gameState});
+        return
+    } else {
+        gameState.round = gameState.round + 1;
+        io.to(roomId).emit('newRound', {round: gameState.round});
+    }
+}
+
 setInterval(() => {
     for (const [roomId, gameState] of Object.entries(knownGameStates)) {
         if (gameState.isTicking) {
-            gameState.round = gameState.round + 1;
             console.log(`Ticking and proceedings to round ${gameState.round} in game ID: ${roomId}`);
-            io.to(roomId).emit('newRound', {round: gameState.round});
-            io.sockets.to(roomId).emit('newRound', {round: gameState.round});
+            processRoundAndProceed(roomId)
         }
     }
 }, 10000)
